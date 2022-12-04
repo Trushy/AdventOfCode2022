8f77ae9905d8b912a784a3abfd0c250d9d85ac75
Antoine Zimmermann <antoine.zimmermann@epitech.eu>
2022-12-04 16:55:14 +0100

    Fourth day made with c++

---
+++ Day4/Part2/main.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int ac, char **av)
+{
+    std::string line;
+    std::ifstream myfile(av[1]);
+
+    int result = 0;
+
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            const string firstElf = line.substr(0, line.find(','));
+            const string secondElf = line.substr(line.find(',') + 1, line.length());
+            const int firstElfFirstInt = stoi(firstElf.substr(0, firstElf.find('-')));
+            const int firstElfSecondInt = stoi(firstElf.substr(firstElf.find('-') + 1, firstElf.length()));
+            const int secondElfFirstInt = stoi(secondElf.substr(0, secondElf.find('-')));
+            const int secondElfSecondInt = stoi(secondElf.substr(secondElf.find('-') + 1, secondElf.length()));
+
+            if ((firstElfFirstInt >= secondElfFirstInt && firstElfFirstInt <= secondElfSecondInt) || (firstElfSecondInt >= secondElfFirstInt && firstElfSecondInt <= secondElfSecondInt))
+                result++;
+            else if ((secondElfFirstInt >= firstElfFirstInt && secondElfFirstInt <= firstElfSecondInt) || (secondElfSecondInt >= firstElfFirstInt && secondElfSecondInt <= firstElfSecondInt))
+                result++;
+        }
+        myfile.close();
+    }
+    else
+        std::cout << "Unable to open file";
+
+    cout << result << endl;
+
+    return 0;
+}
