33d8dfd2528bd0f83f1e482cda3034c275bd075e
Antoine Zimmermann <antoine.zimmermann@epitech.eu>
2022-12-05 19:53:55 +0100

    Fifth day made with c++

---
+++ Day5/Part1/main.cpp
@@ -0,0 +1,64 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <regex>
+using namespace std;
+
+int main(int ac, char **av)
+{
+    std::string line;
+    std::ifstream myfile(av[1]);
+
+    bool isStack = true;
+    vector<list<char>> stacks;
+    string result = "";
+
+    for (int i = 0; i < 9; i++) {
+        list<char> stack;
+        stacks.push_back(stack);
+    }
+
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            cmatch match;
+            regex regex("move ([0-9]+) from ([0-9]+) to ([0-9]+)");
+            regex_search(line.c_str(), match, regex);
+
+            if (line[1] == '1') {
+                isStack = false;
+            }
+            if (isStack) {
+                for (int i = 0; i < 9; i++) {
+                    if (line[i * 4 + 1] != ' ')
+                        stacks[i].push_front(line[i * 4 + 1]);
+                }
+            } else if (match.size() == 4) {
+                const int move = stoi(match[1]);
+                const int from = stoi(match[2]) - 1;
+                const int to = stoi(match[3]) - 1;
+                
+                for (int i = 0; i < move; i++) {
+                    if (stacks[from].size() > 0) {
+                        stacks[to].push_back(stacks[from].back());
+                        stacks[from].pop_back();
+                    }
+                }
+            }
+        }
+        myfile.close();
+    }
+    else
+        std::cout << "Unable to open file";
+
+    for (int i = 0; i < 9; i++) {
+        result.push_back(stacks[i].back());
+    }
+
+    cout << result << endl;
+    return 0;
+}
