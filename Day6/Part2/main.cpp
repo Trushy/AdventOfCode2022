f57e3d8e35c0e93daa6ce9307da7683d71b64e54
Antoine Zimmermann <antoine.zimmermann@epitech.eu>
2022-12-06 12:07:37 +0100

    Sixth day made with c++

---
+++ Day6/Part2/main.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+int main(int ac, char **av)
+{
+    std::string line;
+    std::ifstream myfile(av[1]);
+
+    unordered_map<char, int> characters;
+    bool duplicate = false;
+
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            for (int i = 0; line[i]; i++)
+            {
+                if (line[i + 14]) {
+                    for (int j = 1; j <= 14; j++) {
+                        characters[line[i + j]]++;
+                    }
+
+                    for (auto& it : characters) {
+                        if (it.second > 1) {
+                            duplicate = true;
+                            continue;
+                        }
+                    }
+                    if (!duplicate) {
+                        cout << i + 15 << endl;
+                        return 0;
+                    }
+
+                    duplicate = false;
+                    characters.clear();
+                }
+            }
+        }
+        myfile.close();
+    }
+    else
+        std::cout << "Unable to open file";
+
+    return 0;
+}
